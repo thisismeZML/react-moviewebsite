6c3883efbbeb435d29c0f5d1e3e773f0c60e2cfb
thisismeZML <thatismetheoneudunknow@gmail.com>
2024-07-05 13:47:21 +0630

    update

--- src/hooks/useMovie.js
+++ src/hooks/useMovie.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import axios from "axios";
 import { apiKey } from "../api/apiKey";
 
@@ -10,52 +11,62 @@ const initialState = {
   searchMovies: [],
 };
 
-export const useMovie = create((set) => ({
-  ...initialState,
-  addFavMovies: (movie) =>
-    set((state) => ({ favMovies: [...state.favMovies, movie] })),
-  removeFavMovies: (id) =>
-    set((state) => ({
-      favMovies: state.favMovies.filter((mov) => mov.id !== id),
-    })),
-  addReview: (movieId, newMsg) =>
-    set((state) => ({
-      movies: {
-        ...state.movies,
-        [movieId]: {
-          ...state.movies[movieId],
-          reviews: [...(state.movies[movieId]?.reviews || []), newMsg],
-        },
+export const useMovie = create(
+  persist(
+    (set) => ({
+      ...initialState,
+      addFavMovies: (movie) =>
+        set((state) => ({ favMovies: [...state.favMovies, movie] })),
+      removeFavMovies: (id) =>
+        set((state) => ({
+          favMovies: state.favMovies.filter((mov) => mov.id !== id),
+        })),
+      addReview: (movieId, newMsg) =>
+        set((state) => ({
+          movies: {
+            ...state.movies,
+            [movieId]: {
+              ...state.movies[movieId],
+              reviews: [...(state.movies[movieId]?.reviews || []), newMsg],
+            },
+          },
+        })),
+      deleteReview: (movieId, id) =>
+        set((state) => ({
+          movies: {
+            ...state.movies,
+            [movieId]: {
+              ...state.movies[movieId],
+              reviews: state.movies[movieId].reviews.filter(
+                (msg) => msg.id !== id
+              ),
+            },
+          },
+        })),
+      editReview: (movieId, id, newReview) =>
+        set((state) => ({
+          movies: {
+            ...state.movies,
+            [movieId]: {
+              ...state.movies[movieId],
+              reviews: state.movies[movieId].reviews.map((msg) =>
+                msg.id === id ? { ...msg, review: newReview } : msg
+              ),
+            },
+          },
+        })),
+      toggleSideBar: () => set((state) => ({ isActive: !state.isActive })),
+      searchFilter: async (searchTerm) => {
+        const res = await axios.get(
+          `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`
+        );
+        set({ searchMovies: res.data.results });
+        console.log(res.data);
       },
-    })),
-  deleteReview: (movieId, id) =>
-    set((state) => ({
-      movies: {
-        ...state.movies,
-        [movieId]: {
-          ...state.movies[movieId],
-          reviews: state.movies[movieId].reviews.filter((msg) => msg.id !== id),
-        },
-      },
-    })),
-  editReview: (movieId, id, newReview) =>
-    set((state) => ({
-      movies: {
-        ...state.movies,
-        [movieId]: {
-          ...state.movies[movieId],
-          reviews: state.movies[movieId].reviews.map((msg) =>
-            msg.id === id ? { ...msg, review: newReview } : msg
-          ),
-        },
-      },
-    })),
-  toggleSideBar: () => set((state) => ({ isActive: !state.isActive })),
-  searchFilter: async (searchTerm) => {
-    const res = await axios.get(
-      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`
-    );
-    set({ searchMovies: res.data.results });
-    console.log(res.data);
-  },
-}));
+    }),
+    {
+      name: "movie-store", // unique name
+      getStorage: () => localStorage, // (optional) by default, 'localStorage' is used
+    }
+  )
+);
